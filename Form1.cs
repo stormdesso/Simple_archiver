0707c195b2d6651335e5bd6a611578f0e62700f3
stormdesso <90562074+stormdesso@users.noreply.github.com>
2022-06-30 13:01:55 +0500

    used the pattern model and view separation(MVS)

---
+++ Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab6_v2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Dialog(TextBox field)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            DialogResult result = openFileDialog1.ShowDialog(); // Show the dialog.
+
+            if (result == DialogResult.OK)
+            {
+                string fileName = openFileDialog1.FileName;
+                field.Text = fileName;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)//выбор 1-го файла
+        {
+            Dialog(textBox1);
+        }
+
+        private void button2_Click(object sender, EventArgs e)//выбор 2-го файла
+        {
+            Dialog(textBox2);
+        }
+
+        private void button5_Click(object sender, EventArgs e)//Stop
+        {
+            Archivator.Stop(true);   
+            progressBar1.Value = 0;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+       
+        private void button3_Click(object sender, EventArgs e)//Pack
+        {
+            Archivator arch = new Archivator();
+            arch.notifier += progressBarUpdate;
+            arch.Pack(textBox1.Text, textBox2.Text);            
+        }
+
+        private void button4_Click(object sender, EventArgs e)//Unpack
+        {
+            Archivator arch = new Archivator();
+            arch.notifier += progressBarUpdate;
+            arch.Unpack(textBox1.Text, textBox2.Text);           
+        }
+
+        private void progressBar1_Click(object sender, EventArgs e)
+        {
+              
+        }
+        private void progressBarUpdate(int progress)
+        {
+            progressBar1.Value = progress;
+            Application.DoEvents();            
+        }
+    }
+}
---
+++ Model.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Lab6_v2
+{        
+    class Archivator
+    {
+        public delegate void Notify(int prog);
+        public event Notify notifier;
+
+        private static bool _state = false;
+
+        public void Pack(string FileRes, string FileDest)//исходник , конечный
+        {
+            Progress = 0;
+            notifier?.Invoke(Progress);
+
+            FileStream fs = null;
+            fs = new FileStream(FileRes, FileMode.Open, FileAccess.Read);
+            List<byte> loadData = new List<byte>();   
+            
+            while (fs.Position < fs.Length)//записываем байты из потока в список
+            {                
+                byte sign = (byte)fs.ReadByte();                               
+                loadData.Add(sign);                
+            }            
+            fs.Close();
+            
+            byte numberOfDuplicate = 0;
+            byte numberOfDifferentsBytes = 0;
+            List<byte> archivedData = new List<byte>();
+            List<byte> bufferOfDifferentByte = new List<byte>();
+            for (int i = 0; i < loadData.Count - 1; i++)
+            {
+                if (_state == true)                                    
+                    break;
+                
+                if (loadData[i] == loadData[i + 1])
+                {
+                    if (numberOfDifferentsBytes > 0)
+                    {
+                        archivedData.Add(0);
+                        archivedData.Add(numberOfDifferentsBytes);
+                        archivedData.AddRange(bufferOfDifferentByte);
+                        bufferOfDifferentByte.Clear();
+                        numberOfDifferentsBytes = 0;
+                    }
+                    if (numberOfDuplicate == 255)
+                    {
+                        archivedData.Add(numberOfDuplicate);
+                        archivedData.Add(loadData[i]);
+                        numberOfDuplicate = 0;
+                    }
+                    numberOfDuplicate++;
+                }
+                else 
+                {
+                    if (numberOfDuplicate > 0)
+                    {
+                        numberOfDuplicate++;
+                        archivedData.Add(numberOfDuplicate);
+                        archivedData.Add(loadData[i]);
+                        numberOfDuplicate = 0;
+                    }
+                    else 
+                    {
+                        if (numberOfDifferentsBytes == 255)
+                        {
+                            archivedData.Add(0);
+                            archivedData.Add(numberOfDifferentsBytes);
+                            archivedData.AddRange(bufferOfDifferentByte);
+                            bufferOfDifferentByte.Clear();
+                            numberOfDifferentsBytes = 0;
+                        }
+                        bufferOfDifferentByte.Add(loadData[i]);
+                        numberOfDifferentsBytes++;
+                    }
+                }
+
+                if (i == loadData.Count - 2)
+                {
+                    if (numberOfDuplicate > 0)
+                    {
+                        numberOfDuplicate++;
+                        archivedData.Add(numberOfDuplicate);
+                        archivedData.Add(loadData[i]);
+                        numberOfDuplicate = 0;
+                    }
+                    if (numberOfDifferentsBytes > 0)
+                    {
+                        bufferOfDifferentByte.Add(loadData[i + 1]);
+                        archivedData.Add(0);
+                        numberOfDifferentsBytes++;
+                        archivedData.Add(numberOfDifferentsBytes);
+                        archivedData.AddRange(bufferOfDifferentByte);
+                        bufferOfDifferentByte.Clear();
+                        numberOfDifferentsBytes = 0;
+                    }
+                }
+                
+                double pos = i;
+                double length = loadData.Count-2;
+                Progress = (int)(pos/length * 100);
+                notifier?.Invoke(Progress);               
+            }
+
+            if (_state == false)
+            {
+                FileStream fw = new FileStream(FileDest, FileMode.Create);
+                Byte[] temp = new Byte[archivedData.Count];
+                archivedData.CopyTo(temp);
+                fw.Write(temp, 0, temp.Length);
+                fw.Close();
+            }
+            _state = false;
+        }
+            
+        public void Unpack(string FileRes, string FolderDest)
+        {
+            Progress = 0;
+            notifier?.Invoke(Progress);
+
+            FileStream fs  = new FileStream(FileRes, FileMode.Open);
+            List<byte> loadData = new List<byte>();
+
+            while (fs.Position < fs.Length)//записываем байты из потока в список
+            {
+                byte sign = (byte)fs.ReadByte();
+                loadData.Add(sign);
+            }
+            fs.Close();
+
+            List<byte> extractData = new List<byte>();
+
+            for (int i = 0; i < loadData.Count - 1; i++)
+            {
+                if (_state == true)
+                    break;
+
+                if (loadData[i] == 0)
+                {
+                    i++;
+                    byte sequenceLength = loadData[i];
+                    for (int j = 0; j < sequenceLength; j++)
+                    {
+                        i++;
+                        extractData.Add(loadData[i]);
+                    }
+                }
+                else 
+                {
+                    byte sequenceLength = loadData[i];
+                    i++;
+                    for (int j = 0; j < sequenceLength; j++)                 
+                        extractData.Add(loadData[i]);                    
+                }
+
+                double pos = i;
+                double length = loadData.Count-2;
+                Progress = (int)(pos / length * 100);
+                notifier?.Invoke(Progress);
+            }
+
+            if (_state == false)
+            {
+                FileStream fw = new FileStream(FolderDest, FileMode.Create);
+                Byte[] temp = new Byte[extractData.Count];
+                extractData.CopyTo(temp);
+                fw.Write(temp, 0, temp.Length);
+                fw.Close();
+            }
+            _state = false;
+        }
+
+        public static void Stop(bool state)
+        {
+            _state = state;            
+        }
+        
+        public static int Progress { get; private set; }
+
+    }    
+}
